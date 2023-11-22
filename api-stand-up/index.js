e1b5904dc3c582c39bf9acba0d2b1009d39f267f
beliyandr <beliyandr@gmail.com>
2023-11-22 22:21:34 +0200

    create NODE server

---
+++ api-stand-up/index.js
@@ -0,0 +1,27 @@
+import http from "node:http";
+import fs from "node:fs/promises";
+// const https = require("node:http");
+const PORT = 8080;
+
+http
+  .createServer(async (req, res) => {
+    if (req.method === "GET" && req.url === "/comedians") {
+      try {
+        const data = await fs.readFile("comedians.json", "utf-8");
+        res.writeHead(200, {
+          "Content-type": "text/json; charset=utf8",
+          "Access-Control-Allow-Origin": "*",
+        });
+        res.end(data);
+      } catch (error) {
+        res.writeHead(500, { "Content-type": "text/plains; charset=utf8" });
+        res.end(`Ошибка сервера: ${error}`);
+      }
+    } else {
+      res.writeHead(404, { "Content-type": "text/plains; charset=utf8" });
+      res.end("Not found");
+    }
+  })
+  .listen(8080);
+
+console.log(`Сервер запущен на http://localhost:${PORT}`);
